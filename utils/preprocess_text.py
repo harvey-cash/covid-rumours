06131c13778bb8e21c413600a744903165b3aed1
PalashSurana <palashsurana3@gmail.com>
2021-04-06 05:36:24 +0100

    Changed preprocess_text for only one claim

--- utils/preprocess_text.py
+++ utils/preprocess_text.py
@@ -8,16 +8,15 @@ import gensim
 from nltk.tokenize import sent_tokenize
 from nltk.stem import WordNetLemmatizer
 
-def clean_text(content_collection):
+def clean_text(content):
     wordnet_lemmatizer = WordNetLemmatizer()
     porter = PorterStemmer()
     #lan_porter = LancasterStemmer()
     stop_words = set(stopwords.words('english'))
     clean_content=[]
-    for content in content_collection:
-        #turn string into tokens
-        word_tokens = word_tokenize(content)
-        words = [porter.stem(wordnet_lemmatizer.lemmatize(w.lower())) for w in word_tokens if w not in stop_words] #remove stop words
-        #lowercase, remove stop words and punctuation/numbers
-        clean_content.append( [w for w in words if len(w) > 3] ) 
+    #turn string into tokens
+    word_tokens = word_tokenize(content)
+    words = [porter.stem(wordnet_lemmatizer.lemmatize(w.lower())) for w in word_tokens if w not in stop_words] #remove stop words
+    #lowercase, remove stop words and punctuation/numbers
+    clean_content.append( [w for w in words if len(w) > 3] ) 
     return clean_content
