aa378e7411011f7d4ee63650615ad26d04277f81
Ruxandra <rmmindru1@sheffield.ac.uk>
2021-04-14 16:43:15 +0100

    preprocess clean-up

--- utils/preprocess_text.py
+++ utils/preprocess_text.py
@@ -3,53 +3,64 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.stem.lancaster import LancasterStemmer
-import string  
-import gensim
 from nltk.tokenize import sent_tokenize
 from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import ToktokTokenizer
+import pandas as pd
 
+def lowercase(word_tokens):
+    return [w.lower() for w in word_tokens]
 
-def remove_stopwords(content):
-    stop_words = set(stopwords.words('english'))
-    word_tokens = word_tokenize(content)
-    clean_content = [w.lower() for w in word_tokens if w.lower() not in stop_words]
-    return clean_content
+def remove_stopwords(word_tokens, stop_words):
+    return [w for w in word_tokens if w not in stop_words]
 
-def lemmatize_words(content):
+def lemmatize_words(word_tokens):
     wordnet_lemmatizer = WordNetLemmatizer()
-    word_tokens = word_tokenize(content)
-    clean_content = [wordnet_lemmatizer.lemmatize(w.lower()) for w in word_tokens]
+    return [wordnet_lemmatizer.lemmatize(w) for w in word_tokens]
+
+def stem_words(word_tokens, porter_stem=False, lan_stem=False):
+    if not porter_stem and not lan_stem:
+        return word_tokens
+    porter = PorterStemmer() if porter_stem else LancasterStemmer()
+    return [porter.stem(w) for w in word_tokens]
+
+def remove_small_terms(word_tokens, threshold=3):
+    return [w for w in word_tokens if len(w) > threshold]
+
+def remove_non_alpha_chars(content_collection):
+    only_alpha = content_collection.str.replace(r'[_\W\d]',' ')
+    only_alpha = only_alpha.str.strip()
+    return only_alpha
+
+def remove_non_alpha_chars_from_str(claim):
+    only_alpha = claim.replace(r'[_\W\d]',' ')
+    return only_alpha.strip()
+
+def clean_df(content_collection, stop_words=[], filter_short=3, lemmatize=True, porter_stem=False, lan_stem=False):
+    stop_words = stop_words if stop_words else set(stopwords.words('english'))
+    word_tok = ToktokTokenizer()
+    clean_content = []
+    content_collection = remove_non_alpha_chars(content_collection)
+    for content in content_collection:
+        word_tokens = word_tok.tokenize(content)
+        words = lowercase(word_tokens)
+        words = remove_stopwords(words, stop_words)
+        words = lemmatize_words(words)
+        words = stem_words(words, porter_stem, lan_stem)
+        clean_content.append(remove_small_terms(words, filter_short))
     return clean_content
 
-def stem_words(content):
-    porter = PorterStemmer()
-    #lan_porter = LancasterStemmer()
-    word_tokens = word_tokenize(content)
-    clean_content = [porter.stem(w.lower()) for w in word_tokens]
-    return clean_content
-
-def remove_small_terms(content):
-    SMALL_WORD_THRESHOLD = 3
-    word_tokens = word_tokenize(content)
-    clean_content = [w for w in word_tokens if len(w) > SMALL_WORD_THRESHOLD]
-    return clean_content
-
-# def clean_preprocess(content):
-#     wordnet_lemmatizer = WordNetLemmatizer()
-#     porter = PorterStemmer()
-#     #lan_porter = LancasterStemmer()
-#     stop_words = set(stopwords.words('english'))
-#     clean_content=[]
-#     #turn string into tokens
-#     word_tokens = word_tokenize(content)
-#     words = [porter.stem(wordnet_lemmatizer.lemmatize(w.lower())) for w in word_tokens if w not in stop_words] #remove stop words
-#     #lowercase, remove stop words and punctuation/numbers
-#     clean_content.append( [w for w in words if len(w) > 3] ) 
-#     return clean_content
-
-def clean_all(content):
-    nostop_content = remove_stopwords(content)
-    lemmatized_content = lemmatize_words(" ".join(nostop_content))
-    stemmed_content = stem_words(" ".join(lemmatized_content))
-    clean_content = remove_small_terms(" ".join(stemmed_content))
-    return " ".join(clean_content)
+def clean_str(claim, stop_words=[], filter_short=3, lemmatize=True, porter_stem=False, lan_stem=False):
+    stop_words = stop_words if stop_words else set(stopwords.words('english'))
+    word_tok = ToktokTokenizer()
+    claim = remove_non_alpha_chars_from_str(claim)
+    word_tokens = word_tok.tokenize(claim)
+    words = lowercase(word_tokens)
+    words = remove_stopwords(words, stop_words)
+    words = lemmatize_words(words)
+    words = stem_words(words, porter_stem, lan_stem)
+    return " ".join(remove_small_terms(words, filter_short))
+    
+df = pd.read_csv('../RumourDatabase.csv',encoding='utf8')
+clean_content = clean_str(df['Claim'][0], lemmatize=False, porter_stem=True)
+print(clean_content)
